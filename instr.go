d2b8cc6c55cad8c7a34546047dcf8ed835be01a3
jbirddog <jon.herron@yahoo.com>
2022-03-02 08:52:23 -0500

    Broke out instr

---
+++ instr.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+type LowerContext struct {
+	Inputs    []string
+	Outputs   []string
+	AsmInstrs []AsmInstr
+}
+
+func (c *LowerContext) AppendAsmInstr(i AsmInstr) {
+	c.AsmInstrs = append(c.AsmInstrs, i)
+}
+
+func (c *LowerContext) AppendInput(i string) {
+	c.Inputs = append(c.Inputs, i)
+}
+
+type Instr interface {
+	Lower(*LowerContext)
+	Run(*Environment)
+}
+
+type GoCaller func(*Environment)
+
+type CallGoInstr struct {
+	F GoCaller
+}
+
+func (i *CallGoInstr) Lower(context *LowerContext) {
+}
+
+func (i *CallGoInstr) Run(env *Environment) {
+	i.F(env)
+}
+
+var x8664Mnemonics = map[string]bool{
+	"ret":     true,
+	"syscall": true,
+}
+
+type X8664Instr struct {
+	Mnemonic string
+}
+
+func (i *X8664Instr) Lower(context *LowerContext) {
+	context.AppendAsmInstr(&AsmNoOperandInstr{Mnemonic: i.Mnemonic})
+}
+
+func (i *X8664Instr) Run(env *Environment) {
+	log.Fatal("Cannot run x8664 instructions")
+}
+
+type LiteralIntInstr struct {
+	I int
+}
+
+func (i *LiteralIntInstr) Lower(context *LowerContext) {
+	// TODO this is a hack during prototyping
+	context.AppendInput(strconv.Itoa(i.I))
+}
+
+func (i *LiteralIntInstr) Run(env *Environment) {
+	log.Fatal("Cannot run literal int instructions")
+}
+
+type CallWordInstr struct {
+	Word *Word
+}
+
+func (i *CallWordInstr) Lower(context *LowerContext) {
+	expectedInputs := i.Word.InputRegisters()
+
+	need := len(expectedInputs)
+	have := len(context.Inputs)
+	neededInputs := context.Inputs[have-need:]
+	context.Inputs = context.Inputs[:have-need]
+
+	// TODO probably doesn't need to be backwards
+	for i := need - 1; i >= 0; i-- {
+		if expectedInputs[i] != neededInputs[i] {
+			context.AppendAsmInstr(&AsmBinaryInstr{
+				Mnemonic: "mov",
+				Op1:      expectedInputs[i],
+				Op2:      neededInputs[i],
+			})
+		}
+	}
+
+	calledWordOutputs := i.Word.OutputRegisters()
+	context.Inputs = append(context.Inputs, calledWordOutputs...)
+
+	context.AppendAsmInstr(&AsmCallInstr{Label: i.Word.Name})
+}
+
+func (i *CallWordInstr) Run(env *Environment) {
+	log.Fatal("Cannot run call word instructions")
+}
+
