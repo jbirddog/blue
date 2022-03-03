296b7d77741c48fe9ca42a744d90c035e78f462b
jbirddog <jon.herron@yahoo.com>
2022-03-03 10:35:23 -0500

    nvm utils

--- kernel.go
+++ kernel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 )
 
 func KernelColon(env *Environment) {
@@ -48,6 +49,26 @@ func KernelSemi(env *Environment) {
 	}
 
 	env.Dictionary.HideLocalWords()
+}
+
+func buildRegisterRef(rawRef string, parentRefs []*RegisterRef) *RegisterRef {
+	parts := strings.SplitN(rawRef, ":", 2)
+	partsLen := len(parts)
+
+	if partsLen == 1 {
+		reg := parts[0]
+
+		for _, parentRef := range parentRefs {
+			if parentRef.Name == parts[0] {
+				reg = parentRef.Reg
+				break
+			}
+		}
+
+		return &RegisterRef{Name: parts[0], Reg: reg}
+	}
+
+	return &RegisterRef{Name: parts[0], Reg: parts[1]}
 }
 
 func parseRefs(word *Word, env *Environment) {
