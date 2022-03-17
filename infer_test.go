842dc3deed352acc6eb47ec8525dbcb5f22d1bd1
jbirddog <jon.herron@yahoo.com>
2022-03-17 08:10:55 -0400

    Failing tests for echo's write #2 example

--- infer_test.go
+++ infer_test.go
@@ -85,3 +85,55 @@ func TestEchoReadExample(t *testing.T) {
 		}
 	}
 }
+
+func TestEchoSecondWriteExample(t *testing.T) {
+	// : syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
+	w1 := &Word{
+		Name: "syscall3",
+		Inputs: []*RegisterRef{
+			&RegisterRef{Name: "edi", Reg: "edi"},
+			&RegisterRef{Name: "edx", Reg: "edx"},
+			&RegisterRef{Name: "esi", Reg: "esi"},
+			&RegisterRef{Name: "num", Reg: "eax"},
+		},
+		Outputs: []*RegisterRef{&RegisterRef{Name: "result", Reg: "eax"}},
+	}
+
+	// : write ( len fd -- result ) swap buf 0 syscall3 ;
+	w2 := &Word{
+		Name: "write",
+		Inputs: []*RegisterRef{
+			&RegisterRef{Name: "len", Reg: ""},
+			&RegisterRef{Name: "fd", Reg: ""},
+		},
+		Outputs: []*RegisterRef{&RegisterRef{Name: "wrote", Reg: ""}},
+		Code: []Instr{
+			&SwapInstr{},
+			&LiteralIntInstr{I: 33}, // Dummy value - test is swap
+			&LiteralIntInstr{I: 0},
+			&CallWordInstr{Word: w1},
+		},
+	}
+
+	InferRegisterRefs(w2)
+
+	if len(w2.Inputs) != 2 {
+		t.Fatal("Unexpected input len")
+	}
+
+	if w2.Inputs[0].Reg != "edx" {
+		t.Fatalf("Expected edx, got '%s'", w2.Inputs[0].Reg)
+	}
+
+	if w2.Inputs[1].Reg != "edi" {
+		t.Fatalf("Expected edi, got '%s'", w2.Inputs[1].Reg)
+	}
+
+	if len(w2.Outputs) != 1 {
+		t.Fatal("Unexpected output len")
+	}
+
+	if w2.Outputs[0].Reg != "eax" {
+		t.Fatalf("Expected eax, got '%s'", w2.Outputs[0].Reg)
+	}
+}
