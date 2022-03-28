49772b0e8bb621425619ff4f794aac3222cfd3ab
jbirddog <jon.herron@yahoo.com>
2022-03-27 22:50:00 -0400

    Assume stdout, start of writeln

--- blue/blue.asm
+++ blue/blue.asm
@@ -11,19 +11,56 @@ __blue_3454868101_0:
 	mov eax, 60
 	call __blue_4057121178_0
 
+; : exit.ok ( -- noret )
+__blue_3274522691_0:
+	mov edi, 0
+	call __blue_3454868101_0
+
+; : exit.syserr ( err:eax -- noret )
+__blue_1490145965_0:
+	neg eax
+	mov edi, eax
+	call __blue_3454868101_0
+
+; : unwrap ( result:eax -- value:eax )
+__blue_4055961022_0:
+	cmp eax, 0
+	jge __blue_2157056155_0
+	call __blue_1490145965_0
+
+__blue_2157056155_0:
+	ret
+
 ; : syscall3 ( arg2:esi arg3:edx arg1:edi num:eax -- result:eax )
 __blue_1472650507_0:
 	call __blue_4057121178_0
 	ret
 
-; : write ( buf:esi len:edx fd:edi -- result:eax )
+; : write ( buf len -- result )
 __blue_3190202204_0:
 	mov eax, 1
+	mov edi, 1
 	call __blue_1472650507_0
 	ret
 
-;  TODO unwrap
+; : write! ( buf len -- wrote )
+__blue_3553432007_0:
+	call __blue_3190202204_0
+	call __blue_4055961022_0
+	ret
+
 ;  TODO writeln
+; : lf ( -- )
+__blue_1261555351_0:
+
+db 10
+; : lf ( -- wrote )
+__blue_1261555351_1:
+	mov edx, 1
+	mov esi, __blue_1261555351_0
+	call __blue_3553432007_0
+	ret
+
 ;  TODO this is an example of invalid stack handling due to lodsb/loopne
 ; : find0 ( start:rsi max:rcx -- end:rsi )
 __blue_1805780446_0:
@@ -80,10 +117,9 @@ _start:
 	mov rsi, rdx
 	call __blue_1488600471_0
 	xchg rdx, rsi
-	mov edi, 1
-	call __blue_3190202204_0
-	mov edi, eax
-	call __blue_3454868101_0
+	call __blue_3553432007_0
+	call __blue_1261555351_1
+	call __blue_3274522691_0
 
 ;  argc [rsp] -> rcx for looping over argv
 ;  argv lea rsi, [rsp+8], lodsq loop will load each string ptr into rax
