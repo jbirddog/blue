d123e0277029fa8f8aff58d21b95e71fc8033e64
jbirddog <jon.herron@yahoo.com>
2022-04-08 18:10:59 -0400

    Clean up clear the screen

--- language/examples/rosettacode/clear_the_screen.asm
+++ language/examples/rosettacode/clear_the_screen.asm
@@ -7,29 +7,23 @@ __blue_4057121178_0:
 	ret
 
 ; : exit ( status:edi -- noret )
-__blue_3454868101_1:
+__blue_3454868101_0:
 	mov eax, 60
 	call __blue_4057121178_0
 
-; : exit.ok ( -- noret )
-__blue_3274522691_0:
+; : bye ( -- noret )
+__blue_1911791459_0:
 	mov edi, 0
-	call __blue_3454868101_1
+	call __blue_3454868101_0
 
-; : syscall ( arg2:esi arg3:edx arg1:edi num:eax -- result:eax )
-__blue_4057121178_1:
+; : write ( buf:esi len:edx fd:edi -- )
+__blue_3190202204_0:
+	mov eax, 1
 	call __blue_4057121178_0
 	ret
 
-; : write ( buf len -- )
-__blue_3190202204_1:
-	mov eax, 1
-	mov edi, 1
-	call __blue_4057121178_1
-	ret
-
-; : cls ( -- )
-__blue_59780555_0:
+; : clear-screen ( -- )
+__blue_1703174329_0:
 	jmp __blue_1223589535_0
 
 __blue_855163316_0:
@@ -43,12 +37,13 @@ db 91
 db 72
 db 0
 __blue_1223589535_0:
+	mov edi, 1
 	mov edx, 7
 	mov esi, __blue_855163316_0
-	call __blue_3190202204_1
+	call __blue_3190202204_0
 	ret
 
 ; : _start ( -- noret )
 _start:
-	call __blue_59780555_0
-	call __blue_3274522691_0
+	call __blue_1703174329_0
+	call __blue_1911791459_0
