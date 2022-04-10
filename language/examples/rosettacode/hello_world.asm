d411c193d92c4a2e1ae43bbfde24a35db75f37d7
jbirddog <jon.herron@yahoo.com>
2022-04-09 21:22:22 -0400

    Cleanup

--- language/examples/rosettacode/hello_world.asm
+++ language/examples/rosettacode/hello_world.asm
@@ -22,6 +22,12 @@ __blue_3190202204_0:
 	call __blue_4057121178_0
 	ret
 
+; : print ( buf len -- )
+__blue_372738696_0:
+	mov edi, 1
+	call __blue_3190202204_0
+	ret
+
 ; : greet ( -- )
 __blue_4213039946_0:
 	jmp __blue_1223589535_0
@@ -43,10 +49,9 @@ db 33
 db 10
 db 0
 __blue_1223589535_0:
-	mov edi, 1
 	mov edx, 13
 	mov esi, __blue_855163316_0
-	call __blue_3190202204_0
+	call __blue_372738696_0
 	ret
 
 ; : _start ( -- noret )
