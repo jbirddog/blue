c76fc33cef8f472bcb23dd8e8abfcedeb2ff7e2f
jbirddog <jon.herron@yahoo.com>
2022-04-09 20:16:12 -0400

    Added tutorial3 files

--- language/tutorial/build.sh
+++ language/tutorial/build.sh
@@ -10,3 +10,7 @@ ld -o .build/bin/tutorial1 .build/obj/tutorial1.o
 blue tutorial2.blue
 nasm -f elf64 -o .build/obj/tutorial2.o tutorial2.asm
 ld -o .build/bin/tutorial2 .build/obj/tutorial2.o
+
+blue tutorial3.blue
+nasm -f elf64 -o .build/obj/tutorial3.o tutorial3.asm
+ld -o .build/bin/tutorial3 .build/obj/tutorial3.o
