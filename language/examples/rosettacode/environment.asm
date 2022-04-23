e8be279dd229da0117df8716b3ef7b08652abe7d
jbirddog <jon.herron@yahoo.com>
2022-04-23 17:26:06 -0400

    WIP

--- language/examples/rosettacode/environment.asm
+++ language/examples/rosettacode/environment.asm
@@ -129,6 +129,9 @@ section .bss
 ; 1 resq envp
 
 __blue_2355496332_0: resq 1
+; 1 resq envp-entry
+
+__blue_2301798669_0: resq 1
 section .text
 
 ; : store-envp ( rax -- )
@@ -138,32 +141,70 @@ __blue_2164669320_0:
 	mov [__blue_2355496332_0], rax
 	ret
 
-; : drop-prefix ( namelen:rcx name:rsi entry:rdi -- value:rdi unmatched:rcx )
+; : set-envp-entry ( rax -- )
 
-__blue_2843895655_0:
-	repe cmpsb
+__blue_651937478_0:
+	mov [__blue_2301798669_0], rax
 	ret
 
-; : getenv ( name:rsi len:rcx -- value:rdi )
+; : reset-envp-entry ( -- )
+
+__blue_3243367623_0:
+	mov rax, [__blue_2355496332_0]
+	jmp __blue_651937478_0
+
+; : advance ( rax -- rax )
+
+__blue_667710333_0:
+	add rax, 8
+	ret
+
+; : advance-envp-entry ( -- )
+
+__blue_4288035396_0:
+	mov rax, [__blue_2301798669_0]
+	call __blue_667710333_0
+	ret
+
+; : value-for-name? ( namelen:rcx name:rsi entry:rdi -- value:rdi unmatched:rcx )
+
+__blue_625331137_0:
+
+;  TODO clean up the rot swaps
+
+;  TODO this will match short, like HO will match ME=value
+	repe cmpsb
+	inc dil
+	ret
+
+;  : check-entry ( max:rcx name:rsi entry:rdi -- value:rdi ) 
+
+; 	drop-prefix 0 cmp ' bob xe ;
+
+; : getenv ( name:r8 len:r9 -- value:rdi )
 
 __blue_1306389850_0:
-	mov rax, [__blue_2355496332_0]
+	call __blue_3243367623_0
 
-; : scan-envp ( max:rcx name:rsi next:rax -- value:rdi )
-
-__blue_491392598_0:
-	mov rdi, qword [rax]
-
-; : check-entry ( max:rcx name:rsi entry:rdi -- value:rdi )
+; : check-entry ( name:r8 len:r9 -- value:rdi )
 
 __blue_585535462_0:
-	call __blue_2843895655_0
+	mov rdi, [__blue_2301798669_0]
+	mov rsi, r9
+	mov rcx, r8
+	call __blue_625331137_0
 	cmp rcx, 0
-	jne __blue_2157056155_2
-	call __blue_2261164244_0
+	je __blue_2157056155_2
+	call __blue_4288035396_0
 
 __blue_2157056155_2:
-	jmp __blue_3454868101_0
+	je __blue_2157056155_3
+	call __blue_585535462_0
+
+__blue_2157056155_3:
+	ret
+
+;  : scan-envp ( max:rcx name:rsi next:rax -- value:rdi ) dup @ check-entry ;
 
 ; : _start ( rsp -- noret )
 
@@ -191,9 +232,11 @@ db 69
 db 82
 db 0
 __blue_1223589535_2:
-	mov rcx, 15
-	mov rsi, __blue_855163316_2
+	mov r9, 15
+	mov r8, __blue_855163316_2
 	call __blue_1306389850_0
+	mov rdx, rdi
+	call __blue_372738696_0
 	jmp __blue_1911791459_0
 
 ;  envp @ print bye ;
