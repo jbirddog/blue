0e2b68208545acfc5167420fa74271da80298074
jbirddog <jon.herron@yahoo.com>
2022-04-29 12:32:40 -0400

    Build tutorial with bake

--- language/tutorial/build.sh
+++ language/tutorial/build.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/sh
 
-mkdir -p .build/bin
-mkdir -p .build/obj
+cd ../../tools/bake
+./bootstrap.sh
+cd -
 
-blue tutorial1.blue
-nasm -f elf64 -o .build/obj/tutorial1.o tutorial1.asm
-ld -o .build/bin/tutorial1 .build/obj/tutorial1.o
-
-blue tutorial2.blue
-nasm -f elf64 -o .build/obj/tutorial2.o tutorial2.asm
-ld -o .build/bin/tutorial2 .build/obj/tutorial2.o
-
-blue tutorial3.blue
-nasm -f elf64 -o .build/obj/tutorial3.o tutorial3.asm
-ld -o .build/bin/tutorial3 .build/obj/tutorial3.o
+../../tools/bake/.build/bin/bake build tutorial1.blue
+../../tools/bake/.build/bin/bake build tutorial2.blue
+../../tools/bake/.build/bin/bake build tutorial3.blue
