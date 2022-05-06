cef7d1ce3060f486e6557104781e0878c802f748
jbirddog <jon.herron@yahoo.com>
2022-05-05 22:11:29 -0400

    Fixed test

--- clobbers.go
+++ clobbers.go
@@ -1,6 +1,7 @@
 package main
 
 func UpdateClobbers(clobberSet uint, registerSet uint, indirectRegisterSet uint) uint {
+	clobberSet |= indirectRegisterSet
 	clobberSet &= ^registerSet
 
 	return clobberSet
