361881ef85c8d2f48c404d7c244d2f796bc4411d
jbirddog <jon.herron@yahoo.com>
2022-05-09 12:04:35 -0400

    Fixed third test

--- infer.go
+++ infer.go
@@ -116,8 +116,11 @@ func attemptInference2(env *Environment, word *Word) (bool, []*StackRef, []*Stac
 
 	for _, instr := range env.AsmInstrs {
 		if mov, ok := instr.(*AsmBinaryInstr); ok {
+			if mov.Op1 != "" && mov.Op2 != "" {
+				continue
+			}
+
 			movInstrs = append(movInstrs, mov)
-			log.Printf("%s - f: %s, %s", word.Name, mov.Op1, mov.Op2)
 		}
 	}
 
@@ -132,14 +135,13 @@ func attemptInference2(env *Environment, word *Word) (bool, []*StackRef, []*Stac
 	var inferedInputs []*StackRef
 	var inferedOutputs []*StackRef
 
-	for _, inputMov := range movInstrs[:inputsLen] {
-		inferedInputs = append(inferedInputs, &StackRef{Ref: inputMov.Op1})
-		log.Printf("%s - i: %s, %s", word.Name, inputMov.Op1, inputMov.Op2)
+	for i := inputsLen - 1; i >= 0; i -= 1 {
+		inferedInputs = append(inferedInputs, &StackRef{Ref: movInstrs[i].Op1})
 	}
 
+	// TODO test multiple outputs
 	for _, outputMov := range movInstrs[movsLen-outputsLen:] {
 		inferedOutputs = append(inferedOutputs, &StackRef{Ref: outputMov.Op2})
-		log.Printf("%s - o: %s, %s", word.Name, outputMov.Op1, outputMov.Op2)
 	}
 
 	return true, inferedInputs, inferedOutputs
