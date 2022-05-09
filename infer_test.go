a2160c5f9dfeb6e709f7557cc87d7215074b07ee
jbirddog <jon.herron@yahoo.com>
2022-05-08 22:20:59 -0400

    Refactored tests

--- infer_test.go
+++ infer_test.go
@@ -61,100 +61,52 @@ func TestEchoReadExample(t *testing.T) {
 }
 
 func TestEchoSecondWriteExample(t *testing.T) {
-	// : syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
-	w1 := &Word{
-		Name: "syscall3",
-		Inputs: []*StackRef{
-			&StackRef{Name: "edi", Ref: "edi"},
-			&StackRef{Name: "edx", Ref: "edx"},
-			&StackRef{Name: "esi", Ref: "esi"},
-			&StackRef{Name: "num", Ref: "eax"},
-		},
-		Outputs: []*StackRef{&StackRef{Name: "result", Ref: "eax"}},
-	}
+	w := parseForLatest(`
+		1 resb buf
+		: syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
+		: write ( len fd -- result ) swap buf 0 syscall3 ;
+	`)
 
-	// : write ( len fd -- result ) swap buf 0 syscall3 ;
-	w2 := &Word{
-		Name: "write",
-		Inputs: []*StackRef{
-			&StackRef{Name: "len", Ref: ""},
-			&StackRef{Name: "fd", Ref: ""},
-		},
-		Outputs: []*StackRef{&StackRef{Name: "wrote", Ref: ""}},
-		Code: []Instr{
-			&SwapInstr{},
-			&LiteralIntInstr{I: 33}, // Dummy value - test is swap
-			&LiteralIntInstr{I: 0},
-			&CallWordInstr{Word: w1},
-		},
-	}
+	InferStackRefs(w)
 
-	InferStackRefs(w2)
-
-	if len(w2.Inputs) != 2 {
+	if len(w.Inputs) != 2 {
 		t.Fatal("Unexpected input len")
 	}
-
-	if w2.Inputs[0].Ref != "edx" {
-		t.Fatalf("Expected edx, got '%s'", w2.Inputs[0].Ref)
+	if w.Inputs[0].Ref != "edx" {
+		t.Fatalf("Expected edx, got '%s'", w.Inputs[0].Ref)
+	}
+	if w.Inputs[1].Ref != "edi" {
+		t.Fatalf("Expected edi, got '%s'", w.Inputs[1].Ref)
 	}
 
-	if w2.Inputs[1].Ref != "edi" {
-		t.Fatalf("Expected edi, got '%s'", w2.Inputs[1].Ref)
-	}
-
-	if len(w2.Outputs) != 1 {
+	if len(w.Outputs) != 1 {
 		t.Fatal("Unexpected output len")
 	}
-
-	if w2.Outputs[0].Ref != "eax" {
-		t.Fatalf("Expected eax, got '%s'", w2.Outputs[0].Ref)
+	if w.Outputs[0].Ref != "eax" {
+		t.Fatalf("Expected eax, got '%s'", w.Outputs[0].Ref)
 	}
 }
 
 func TestWriteDropsResult(t *testing.T) {
-	// : syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
-	w1 := &Word{
-		Name: "syscall3",
-		Inputs: []*StackRef{
-			&StackRef{Name: "edi", Ref: "edi"},
-			&StackRef{Name: "edx", Ref: "edx"},
-			&StackRef{Name: "esi", Ref: "esi"},
-			&StackRef{Name: "num", Ref: "eax"},
-		},
-		Outputs: []*StackRef{&StackRef{Name: "result", Ref: "eax"}},
-	}
+	w := parseForLatest(`
+		1 resb buf
+		: syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
+		: write ( fd len -- ) swap buf 0 syscall3 drop ;
+	`)
 
-	// : write ( fd len -- ) swap buf 0 syscall3 drop ;
-	w2 := &Word{
-		Name: "write",
-		Inputs: []*StackRef{
-			&StackRef{Name: "fd", Ref: ""},
-			&StackRef{Name: "len", Ref: ""},
-		},
-		Code: []Instr{
-			&LiteralIntInstr{I: 33}, // Dummy value - test is drop
-			&LiteralIntInstr{I: 0},
-			&CallWordInstr{Word: w1},
-			&DropInstr{},
-		},
-	}
+	InferStackRefs(w)
 
-	InferStackRefs(w2)
-
-	if len(w2.Inputs) != 2 {
+	if len(w.Inputs) != 2 {
 		t.Fatal("Unexpected input len")
 	}
-
-	if w2.Inputs[0].Ref != "edi" {
-		t.Fatalf("Expected edi, got '%s'", w2.Inputs[0].Ref)
+	if w.Inputs[0].Ref != "edx" {
+		t.Fatalf("Expected edx, got '%s'", w.Inputs[0].Ref)
+	}
+	if w.Inputs[1].Ref != "edi" {
+		t.Fatalf("Expected edi, got '%s'", w.Inputs[1].Ref)
 	}
 
-	if w2.Inputs[1].Ref != "edx" {
-		t.Fatalf("Expected edx, got '%s'", w2.Inputs[1].Ref)
-	}
-
-	if len(w2.Outputs) != 0 {
+	if len(w.Outputs) != 0 {
 		t.Fatal("Unexpected output len")
 	}
 }
