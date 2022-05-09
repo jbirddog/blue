4466967eba348542a72e1687bf9215f01707c85a
jbirddog <jon.herron@yahoo.com>
2022-05-09 08:00:24 -0400

    Migrate some tests to test cases

--- infer_test.go
+++ infer_test.go
@@ -4,59 +4,47 @@ import (
 	"testing"
 )
 
-func TestDoesNothingIfRefsAreComplete(t *testing.T) {
-	w := parseForLatest(": bob ( eax -- edi ) ;")
-
-	InferStackRefs(w)
-
-	if w.Inputs[0].Ref != "eax" {
-		t.Fatalf("Unexpected inputs reg")
-	}
-
-	if w.Outputs[0].Ref != "edi" {
-		t.Fatalf("Unexpected output reg")
-	}
-}
-
-func TestInfersBasicInputRefs(t *testing.T) {
-	w := parseForLatest(`
+func TestInference(t *testing.T) {
+	cases := []struct {
+		codeUnderTest   string
+		expectedInputs  []string
+		expectedOutputs []string
+	}{
+		{": bob ( eax -- edi ) ;", []string{"eax"}, []string{"edi"}},
+		{`
 		: bob ( eax -- ) ;
 		: sue ( joe -- ) bob ;
-	`)
-
-	InferStackRefs(w)
-
-	if w.Inputs[0].Ref != "eax" {
-		t.Fatalf("Failed to infer eax")
-	}
-}
-
-func TestEchoReadExample(t *testing.T) {
-	w := parseForLatest(`
+	`, []string{"eax"}, nil},
+		{`
 		: syscall3 ( edi edx esi num:eax -- result:eax ) syscall ;
 		: read ( fd len buf -- result ) 0 syscall3 ;
-	`)
-
-	InferStackRefs(w)
-
-	if len(w.Inputs) != 3 {
-		t.Fatalf("Expected 3 inputs got %d", len(w.Inputs))
-	}
-	if w.Inputs[0].Ref != "edi" {
-		t.Fatalf("Expected edi got '%s'", w.Inputs[0].Ref)
-	}
-	if w.Inputs[1].Ref != "edx" {
-		t.Fatalf("Expected edx got '%s'", w.Inputs[1].Ref)
-	}
-	if w.Inputs[2].Ref != "esi" {
-		t.Fatalf("Expected esi got '%s'", w.Inputs[2].Ref)
+	`, []string{"edi", "edx", "esi"}, []string{"eax"}},
 	}
 
-	if len(w.Outputs) != 1 {
-		t.Fatalf("Expected 1 outputs got %d", len(w.Outputs))
-	}
-	if w.Outputs[0].Ref != "eax" {
-		t.Fatalf("Expected eax got '%s'", w.Outputs[0].Ref)
+	for _, c := range cases {
+		w := parseForLatest(c.codeUnderTest)
+
+		InferStackRefs(w)
+
+		if len(w.Inputs) != len(c.expectedInputs) {
+			t.Fatalf("Expected %d inputs got %d", len(w.Inputs), len(c.expectedInputs))
+		}
+
+		for i, input := range w.Inputs {
+			if input.Ref != c.expectedInputs[i] {
+				t.Fatalf("Expected '%s' got '%s'", c.expectedInputs[i], input.Ref)
+			}
+		}
+
+		if len(w.Outputs) != len(c.expectedOutputs) {
+			t.Fatalf("Expected %d outputs got %d", len(w.Outputs), len(c.expectedOutputs))
+		}
+
+		for i, output := range w.Outputs {
+			if output.Ref != c.expectedOutputs[i] {
+				t.Fatalf("Expected '%s' got '%s'", c.expectedOutputs[i], output.Ref)
+			}
+		}
 	}
 }
 
