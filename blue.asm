8d4c297a10a8d843de15a7b3db8f5f5174b3f30e
Jon Herron <jon.herron@yahoo.com>
2022-12-22 16:19:12 -0500

    POC

--- blue.asm
+++ blue.asm
@@ -1,6 +1,18 @@
 format ELF64 executable 3 ; fasm q: why 3? can omit or change
 
-; needed in asm
+;
+; configuration
+;
+
+config:
+
+.cell_size = 8
+.input_buffer_size = 4096
+.max_data_stack_items = 32
+.max_user_words = 1024
+.word_code_size = 32
+
+; needed in asm - generic words to support
 ;
 ; * read until c from input buffer
 ; * create dictionary entry
@@ -10,27 +22,55 @@ format ELF64 executable 3 ; fasm q: why 3? can omit or change
 ; * write bytes to code buffer
 ; * drop/dup/swap
 
-; need design
-;
-; * call word instructions
-
 segment readable writeable executable
+
+;
+; code buffer
+;
+
+code_buffer:
+.cap = config.word_code_size * config.max_user_words
+rb .cap
+.end:
+
+segment readable writeable
+
+;
+; compile time data stack
+;
+
+data_stack:
+.cap = config.cell_size * config.max_data_stack_items
+rb .cap
+.end:
+
+;
+; dictionary
+;
 
 ; dictionary entry
 ;
 ; * want flags first to test flags?
 ; * want to track call count?
 ;
-; 1  - flags (hidden,immediate,inline,noret)
-; 7  - key
-; 8  - pointer to code buffer
+; 8  - state & flags (hidden,immediate,inline,noret)
+; 8  - key
+; 8  - code
+; 8  - reserved
 ; ...
 
-dictionary rb 1024
+dictionary: 
+.entry_size = 32
+.latest rq 1
+.next rq 1
+.start:
+.core:
+.user rb config.max_user_words * .entry_size
+.end:
 
 segment readable executable
 
-main:
+entry $
 	mov edx, msg_size
 	
 	lea rsi, [msg]
@@ -38,7 +78,7 @@ main:
 	mov eax, 1
 	syscall
 
-	lea rdi, [dictionary]
+	lea rdi, [code_buffer]
 	mov byte [edi], 0xff
 	inc rdi
 	mov byte [edi], 0xc7
@@ -55,15 +95,15 @@ main:
 	inc rdi
 
 	xor edi, edi
-	lea rcx, [dictionary]
-	add rcx, 2
+	;mov edi, [code_buffer.in]
+	lea rcx, [code_buffer]
+	;add rcx, 2
 	call rcx
 	mov eax, 60
 	syscall
 
-entry main
 
 segment readable 
 
-msg db 'blue',0xA
+msg db 'blue compiler (fasm edition)',0xA
 msg_size = $-msg
