68b2ef7bac7c0e53db7820906225441235936d92
Jon Herron <jon.herron@yahoo.com>
2024-04-12 07:46:37 -0400

    Small setup

--- driver.asm
+++ driver.asm
@@ -3,37 +3,49 @@
 ;;; 
 ;;; 
 
-	section .dict write
-
-__dict:
-.exit:
-	
+	section codebuf write exec
 	
 ;;;
 ;;; 
 ;;; 
 
-	section .codebuf write exec
-
 __codebuf:
-
-__builtin:
+	.here dq 0
+	.start:
 	.exit:
-	; : exit (( status edi  -- )) noret
+	;; : exit (( status edi  -- )) 60 syscall ; noret
 	mov eax, 60
 	db 0x0f, 0x05			; syscall
 
+	.user:
+	times 4096 db 0
+
 ;;;
 ;;; 
 ;;; 
 
-	section .text
+__dict:
+	.here dq 0
+	.start:
+	.exit:
+
+	.user:
+	times 4096 db 0
+
+;;;
+;;; 
+;;; 
 
 	global _start
 
 _start:
+	mov rax, __codebuf.user	
+	mov [__codebuf.here], rax
+	
+	;; 3 4 add exit 
 	mov eax, 3
 	add eax, 4
+	
 	mov edi, eax
-	call __builtin.exit
+	jmp __codebuf.exit
 	
