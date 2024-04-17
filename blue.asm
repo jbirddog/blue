5d4f07d840af2254d1cf0a60cb41ec5af456e0fc
Jon Herron <jon.herron@yahoo.com>
2024-04-17 06:44:08 -0400

    Small setup

--- blue.asm
+++ blue.asm
@@ -9,9 +9,24 @@
 ;;; 
 ;;;
 
+__stack:
+	.here dq 0
+	.start times 32 dq 0
+	.end:
+
+	.push:
+	;; assumes value in rax
+	mov rsi, [.here]
+	stosq
+	mov [.here], rsi
+	ret
+
 __codebuf:
 	.here dq 0
+	
 	.start:
+	.entry_jmp db 0xE9
+	.entry dq 0
 
 	.syscall_1:
 	;; : syscall/1 (( num eax -- result eax )) ... ; inline
@@ -22,10 +37,10 @@ __codebuf:
 	mov eax, 60
 	jmp .syscall_1
 
-	.add_1:
-	;; : add/1 (( n eax -- n+1 eax )) ... ; inline
+	.add1:
+	;; : add1 (( n eax -- n+1 eax )) ... ; inline
 	inc eax
-	ret 			; how does `ret` work with inline
+	ret
 
 	.__user:
 	times 4096 db 0
@@ -49,6 +64,9 @@ __dict:
 	global _start
 
 _start:
+	mov rsi, __stack.start
+	mov [__stack.here], rsi
+	
 	mov rsi, __codebuf.__user	
 	mov [__codebuf.here], rsi
 
@@ -61,19 +79,17 @@ _start:
 	;; want to simulate the program:
 	;;
 	;; 6 add1
-	;; : _start exit ;
+	;; : _start exit ; entry
 	;;
-	;; assumes `add1` and `exit` are built in
+	;; demo assumes `add1` and `exit` are already defined
 	;; `6 add1` is run at compile time and results in `7` being on the stack
+	;;
 	;;
 	;; the final binary output would roughly be the equivalent of:
 	;;
-	;; mov eax, 7
-	;; mov edi, eax
+	;; mov rdi, 7
 	;; mov eax, 60
 	;; syscall
-	;;
-	;; * some future peephole optimizer could later reduce the first two lines
 	;;
 	;; the output should be a binary file containing the machine code for the
 	;; above unoptimized assembly. This should be able to be included in a
@@ -90,17 +106,31 @@ _start:
 	;; compile the user program
 	;; 
 	
-	;; 6 add1 exit 
-	mov eax, 6
-	call __codebuf.add_1
-	
-	mov edi, eax
-	jmp __codebuf.exit
+	;; `6 add1` executed at compile time 
+	push 6
+	pop rax
+	call __codebuf.add1
+
+	;; stack now indicates there is an immediate value in `eax`. when moving into
+	;; into `edi` for `exit` the value of `eax` needs to be compiled. for now just
+	;; move the full register but later respect the size from the register name.
 
 	;; 
-	;; __codebuf.here is set to point to the instructions for _start
-	;; then write the instructions to out.bin
+	;; TODO: compile into __codebuf:
+	;; 
+	;; BF07000000 - mov rdi, 7
+	;; E9A6DFFFFF - jmp __codebuf.exit
+	;; 
+
+	;; 
+	;; set the `entry` to `_start`'s code. for this demo this just happens to be
+	;; the start of the user section of the code buffer, but really `entry` would
+	;; find the previously defined word and use its code.
 	;;
 
-	
-	
+	;;
+	;; write the code buffer to out.bin and exit
+	;; 
+
+	xor edi, edi
+	jmp __codebuf.exit
