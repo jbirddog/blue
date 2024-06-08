b7c3a14648c30865ad4eb0c62230c5a8068fbbc3
Jon Herron <jon.herron@yahoo.com>
2024-06-08 16:20:17 -0400

    WIP

--- blue.asm
+++ blue.asm
@@ -4,10 +4,21 @@ include "const.inc"
 	
 segment readable writeable
 
+_code_buffer:
+	.length = 4096
+	.base dq 1
+	.here dq 1
+
 _data_stack:
 	.length = 4096
 	.base dq 1
 	.here dq 1
+
+_dictionary:
+	.length = 4096
+	.base dq 1
+	.here dq 1
+	.latest dq 1
 	
 segment readable executable
 
@@ -44,6 +55,21 @@ macro mmap_buffer len, prot {
 	call mmap.buffer
 }
 
+code_buffer:
+	.init:
+	mmap_buffer _code_buffer.length, PROT_RWX
+	
+	mov [_code_buffer.base], rax
+	mov [_code_buffer.here], rax
+	ret
+
+	.deinit:
+	mov esi, _code_buffer.length
+	mov rdi, [_code_buffer.base]
+	call mmap.unmap
+	ret
+	
+
 data_stack:
 	.init:
 	mmap_buffer _data_stack.length, PROT_RW
@@ -79,9 +105,41 @@ data_stack:
 	mov edi, 3
 	mov eax, 60
 	syscall
+
+dictionary:
+	;
+	; entry:
+	;
+	; 08 - 1 byte flags
+	;    - 1 byte word length
+	;    - 6 bytes first chars
+	; 08 - prev addr
+	; 08 - code addr
+	; 08 - 1 byte length of remaining word chars
+	;    - 1 byte length of code
+	;    - 6 bytes reserved
+	; ?? - remaining word chars
+	; ?? - stack effects
+	;
+
+	.init:
+	mmap_buffer _dictionary.length, PROT_RW
+
+	mov [_dictionary.base], rax
+	mov [_dictionary.here], rax
+	mov [_dictionary.latest], 0
+	ret
+
+	.deinit:
+	mov esi, _dictionary.length
+	mov rdi, [_dictionary.base]
+	call mmap.unmap
+	ret
 	
 entry $
+	call code_buffer.init
 	call data_stack.init
+	call dictionary.init
 
 	mov eax, 6
 	call data_stack.push
@@ -90,12 +148,12 @@ entry $
 	mov eax, 7
 	call data_stack.push
 	call data_stack.pop
+
+	call code_buffer.deinit
+	call data_stack.deinit
+	call dictionary.deinit
 	
 	xor edi, edi
 	mov edi, eax
 	mov eax, 60
 	syscall
-
-	;; move back up, using exit codes to debug...
-	call data_stack.deinit
-	
