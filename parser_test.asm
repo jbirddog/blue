6554842bffdbb86cf8d8e90287982f7eeaa987de
Jon Herron <jon.herron@yahoo.com>
2024-06-18 20:02:11 -0400

    WIP

--- parser_test.asm
+++ parser_test.asm
@@ -1,20 +1,22 @@
 format elf64 executable 3
 
-macro tc num, tib, tib_len, tib_in, word_len {
+macro tc1 tib, tib_len, tib_in, word_len {
 	mov	[_blue.tib], tib
 	mov	[_blue.tib_len], tib_len
 	mov	[_blue.tib_in], tib_in
 	call	parser_next_word
 
-	mov	edi, num
+	inc	[test_num]
 	mov	al, [_blue.word_len]
 	cmp	al, word_len
-	jne	exit
+	jne	failure
 }
 
 segment readable writeable
 
 include "defs.inc"
+
+test_num db 1
 
 segment readable executable
 
@@ -22,10 +24,16 @@ include "linux.inc"
 include "parser.inc"
 
 entry $
-	tc	1, nothing, nothing.length, 0, 0
-	tc	2, just_ws, just_ws.length, 0, 0
+	mov	[test_num], 0
+	
+	tc1	nothing, nothing.length, 0, 0
+	
+	tc1	just_space, just_space.length, 0, 0
+	tc1	just_tab, just_tab.length, 0, 0
+	tc1	just_newline, just_newline.length, 0, 0
+	tc1	some_ws, some_ws.length, 0, 0
 
-	;tc	2, just_a_number, just_a_number.length, 0, 1
+	;tc1	just_a_number, just_a_number.length, 0, 1
 
 	xor	edi, edi
 
@@ -33,14 +41,30 @@ exit:
 	mov	eax, 60
 	syscall
 
+failure:
+	mov	dil, [test_num]
+	jmp	exit
+
 segment readable
 
 nothing:
 	.length = $ - nothing
 
-just_ws:
+just_space:
+	db 32
+	.length = $ - just_space
+
+just_tab:
+	db 9
+	.length = $ - just_tab
+
+just_newline:
+	db 10
+	.length = $ - just_newline
+
+some_ws:
 	db 32, 9, 10, 32
-	.length = $ - just_ws
+	.length = $ - some_ws
 
 just_a_number:
 	db '6'
