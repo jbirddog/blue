0bae3ad385bf0c58758812690e9522586d94bda3
Jon Herron <jon.herron@yahoo.com>
2024-06-23 12:22:09 -0400

    WIP

--- to_number_test.asm
+++ to_number_test.asm
@@ -1,10 +1,9 @@
 format elf64 executable 3
 
-macro tc1 tib, tib_len, base, result, expected {
+macro tc1 tib, tib_len, result, expected {
 	mov	[_blue.tib], tib
 	mov	[_blue.tib_len], tib_len
 	mov	[_blue.tib_in], 0
-	mov	byte [_blue.base], base
 
 	call	parser_next_word
 	call	to_number
@@ -34,11 +33,11 @@ entry $
 	mov	[test_num], 0
 	mov	[_blue.base], 10
 
-	tc1	not_number, 4, 10, FAILURE, 0
-	tc1	zero, 1, 10, SUCCESS, 0
-	tc1	one, 1, 10, SUCCESS, 1
-	tc1	thirteen, 2, 10, SUCCESS, 13
-	tc1	thousand, 4, 10, SUCCESS, 1000
+	tc1	not_number, 4, FAILURE, 0
+	tc1	zero, 1, SUCCESS, 0
+	tc1	one, 1, SUCCESS, 1
+	tc1	thirteen, 2, SUCCESS, 13
+	tc1	thousand, 4, SUCCESS, 1000
 
 	xor	edi, edi
 	
