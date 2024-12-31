4f5223786d0bb9136cd3451e58ec89344a6c8274
Jon Herron <jon.herron@yahoo.com>
2024-12-30 19:09:17 -0500

    WIP

--- v3.asm
+++ v3.asm
@@ -2,11 +2,13 @@ format elf64 executable 3
 
 segment readable writable executable
 
-
-DATA_STACK_ELEMS = 64
+CELL_SIZE = 8
+DATA_STACK_CELLS = 64
 USER_CODE_BUFFER_SIZE = 1024
-WORD_SIZE = 16
-WORD_SIZE_SHL = 4
+
+; bytes to cells, cells to bytes
+macro _b2c b { shr b, 3 }
+macro _c2b c { shl c, 3 }
 
 read_input:
 	;
@@ -28,17 +30,17 @@ read_input_b:
 	mov	edx, 1
 	jmp	read_input
 
-push_tib:
+data_stack_depth:
+	mov	rax, [data_stack_here]
+	sub	rax, data_stack
+	_b2c	rax
+	ret
+
+data_stack_push_tib:
 	mov	rax, [tib]
 	mov	rdi, [data_stack_here]
 	stosq
 	mov	[data_stack_here], rdi
-	ret
-
-data_stack_depth:
-	mov	rax, [data_stack_here]
-	sub	rax, data_stack
-	shr	rax, 3
 	ret
 	
 	
@@ -51,7 +53,7 @@ entry $
 	call	read_input_b
 
 	mov	rax, [tib]
-	shl	rax, WORD_SIZE_SHL
+	_c2b	rax
 	add	rax, code_buffer
 	call	rax
 
@@ -66,21 +68,25 @@ entry $
   	;db 0xb8, 0x3c, 0x00, 0x00, 0x00
   	;db 0x0f, 0x05
 
-; TODO: macro op that takes body
-macro _pad op {
-	assert ($ - op) <= WORD_SIZE
-	times (WORD_SIZE - ($ - op)) db 0
-	assert ($ - op) = WORD_SIZE
-}
-	
-code_buffer:
-_op_00:
+
+_00:
 	; ( -- b ) - read byte from input, push on the data stack
 	call	read_input_b
-	call	push_tib
+	call	data_stack_push_tib
 	ret
 
-	_pad _op_00
+macro _op l {
+	._op##l:
+	call	l
+	ret
+	
+	assert ($ - ._op##l) <= CELL_SIZE
+	times (CELL_SIZE - ($ - ._op##l)) db 0
+	assert ($ - ._op##l) = CELL_SIZE
+}
+
+code_buffer:
+	_op	_00	; ( -- b ) read byte from input, push on the data stack
 
 ;
 ; everything below here needs to be r* else bytes will be in the binary
@@ -91,4 +97,4 @@ rb USER_CODE_BUFFER_SIZE
 tib rq 1
 data_stack_here rq 1
 
-data_stack rq DATA_STACK_ELEMS
+data_stack rq DATA_STACK_CELLS
