ef737c88739fa8532d80b46468e0bd5872833dfb
Jon Herron <jon.herron@yahoo.com>
2024-12-31 22:05:24 -0500

    WIP

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/sh
 
+mkdir -p bin
+
 fasm blue.asm bin/blue
 
-cat
+cat \
   x86_64/elf/pre.bs1 \
-  demo.bs1 \
+  examples/x86_64/linux/exit7.bs1 \
   x86_64/elf/post.bs1 \
   | grep -v "#" \
   | xxd -p -r \
-  | ./bin/blue > bin/demo && chmod +x bin/demo
+  | ./bin/blue > bin/exit7 && chmod +x bin/exit7
