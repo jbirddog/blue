adbed847b4f9e221bda2d8620ad034c865880ef2
Jon Herron <jon.herron@yahoo.com>
2025-01-08 14:04:20 -0500

    WIP

--- examples/blang/blang.pl
+++ examples/blang/blang.pl
@@ -4,10 +4,11 @@ use strict;
 use warnings;
 
 my %kw_to_op = qw(
-  halt 0x00
+  halt 00
 );
 
-my $prog = do { local $/; <STDIN> };
-my @tokens = split /\s/, $prog;
+my $prog = join " ", grep { /^[^#]/ } <STDIN>;
+my @tokens = split /\s+/, $prog;
+my @bytes = map { chr hex($kw_to_op{$_} || $_) } @tokens;
 
-print @tokens;
+print @bytes;
