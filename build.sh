4c1268cb1cb3ff1f6460d14e22b189d821befc1e
Jon Herron <jon.herron@yahoo.com>
2025-01-13 20:11:55 -0500

    WIP

--- build.sh
+++ build.sh
@@ -17,10 +17,15 @@ fasm bluevm.asm bin/bluevm
 
 echo "* Building bs0 files"
 
+./examples/blang/blang.pl < tests/halt.bl > obj/test_halt.bs0
 ./examples/blang/blang.pl < tests/ops.bl > obj/test_ops.bs0
 
 echo "* Running bs0 test cases"
 
+echo "** Test Halt"
+./bin/bluevm < obj/test_halt.bs0
+
+echo "** Test Ops"
 ./bin/bluevm < obj/test_ops.bs0
 
 echo "* Done"
