11268602b8c805c7c96e524eaf7b1e7531627635
Jon Herron <jon.herron@yahoo.com>
2025-01-16 20:12:46 -0500

    WIP

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ fasm bluevm.asm bin/bluevm
 echo "* Building bs0 files"
 
 ./examples/blang/blang.pl < tests/exit.bl > obj/test_exit.bs0
+./examples/blang/blang.pl < tests/bc.bl > obj/test_bc.bs0
 ./examples/blang/blang.pl < tests/custom_op.bl > obj/test_custom_op.bs0
 ./examples/blang/blang.pl < tests/ops.bl > obj/test_ops.bs0
 
@@ -25,6 +26,9 @@ echo "* Running bs0 test cases"
 
 echo "** Test Exit"
 ./bin/bluevm < obj/test_exit.bs0
+
+echo "** Test Compiling and Calling Bytecode"
+./bin/bluevm < obj/test_bc.bs0
 
 echo "** Test Custom Opcode"
 ./bin/bluevm < obj/test_custom_op.bs0
