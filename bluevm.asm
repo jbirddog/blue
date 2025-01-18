6d4892d761c0cc00c363a8af2038b196b57a6e43
jbirddog <100367399+jbirddog@users.noreply.github.com>
2025-01-18 12:24:49 -0500

    Merge mem alloc and init (#93)

--- bluevm.asm
+++ bluevm.asm
@@ -27,39 +27,34 @@ include "opcodes.inc"
 include "interpreter.inc"
 include "sys.inc"
 
-mem_alloc:
+mem_alloc_init:
 	mov	esi, MEM_SIZE
 	call	mmap_rwx
 
 	mov	[mem], rax
 	
-	ret
-
-vm_data_init:
-	mov	rdi, [mem]
-	
-	lea	rsi, [rdi + INPUT_BUFFER_OFFSET]
+	lea	rsi, [rax + INPUT_BUFFER_OFFSET]
 	mov	[instruction_pointer], rsi
 	mov	[input_buffer], rsi
 	
-	lea	rsi, [rdi + RETURN_STACK_OFFSET]
+	lea	rsi, [rax + RETURN_STACK_OFFSET]
 	mov	[return_stack], rsi
 	mov	[return_stack_here], rsi
 	mov	[return_stack_size], RETURN_STACK_SIZE
 	
-	lea	rsi, [rdi + DATA_STACK_OFFSET]
+	lea	rsi, [rax + DATA_STACK_OFFSET]
 	mov	[data_stack], rsi
 	mov	[data_stack_here], rsi
 	mov	[data_stack_size], DATA_STACK_SIZE
 	
-	lea	rsi, [rdi + OPCODE_MAP_OFFSET]
+	lea	rsi, [rax + OPCODE_MAP_OFFSET]
 	mov	[opcode_map], rsi
 
-	lea	rsi, [rdi + VM_ADDRS_OFFSET]
-	mov	rax, opcode_handler_call
-	mov	[rsi], rax
+	lea	rsi, [rax + VM_ADDRS_OFFSET]
+	mov	rcx, opcode_handler_call
+	mov	[rsi], rcx
 	
-	lea	rsi, [rdi + CODE_BUFFER_OFFSET]
+	lea	rsi, [rax + CODE_BUFFER_OFFSET]
 	mov	[code_buffer], rsi
 	mov	[code_buffer_here], rsi
 	mov	[code_buffer_size], CODE_BUFFER_SIZE
@@ -81,7 +76,6 @@ read_boot_code:
 	ret
 
 entry $
-	call	mem_alloc
-	call	vm_data_init
+	call	mem_alloc_init
 	call	read_boot_code	
 	call	outer_interpreter
