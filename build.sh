d68039e1c1f932611d1920c98df827b0e76b0b2b
Jon Herron <jon.herron@yahoo.com>
2025-01-20 15:38:35 -0500

    WIP

--- build.sh
+++ build.sh
@@ -13,8 +13,24 @@ mkdir -p obj
 
 echo "* Building BlueVM v5 x86_64/linux"
 
-fasm v5.asm bin/tmp
-cat bin/tmp bin/tmp > bin/v5
+fasm v5.asm bin/driver
+fasm opcode_tbl.asm bin/opcode_tbl
+
+echo "7FF: 00" | xxd -r > bin/xtd_opcode_tbl
+echo "7FF: 00" | xxd -r > bin/code_buffer
+echo "3FF: 00" | xxd -r > bin/return_stack
+echo "3FF: 00" | xxd -r > bin/data_stack
+echo "3FF: 00" | xxd -r > bin/input_buffer
+
+cat \
+  bin/driver \
+  bin/opcode_tbl \
+  bin/xtd_opcode_tbl \
+  bin/return_stack \
+  bin/data_stack \
+  bin/code_buffer \
+  bin/input_buffer \
+> bin/v5
+
 chmod +x bin/v5
 ./bin/v5
-
