2f15a7455845a6397354cab180d2973efa8cc467
Jon Herron <jon.herron@yahoo.com>
2025-01-21 10:28:43 -0500

    WIP

--- bluevm.asm
+++ bluevm.asm
@@ -12,26 +12,25 @@ include "defs.inc"
 include "stack.inc"
 include "opcodes.inc"
 include "interpreter.inc"
-include "sys.inc"
 
-mem_alloc_init:
-	mov	esi, MEM_SIZE
-	call	mmap_rwx
+; expects status in edi
+exit:
+	mov	eax, 60
 
-	mov	[mem], rax
-
-	;lea	rsi, [rax + VM_ADDRS_OFFSET]
-	;mov	rcx, opcode_handler_call
-	;mov	[rsi], rcx
+syscall_or_die:
+	syscall
 	
-	;lea	rsi, [rax + CODE_BUFFER_OFFSET]
-	;mov	[code_buffer], rsi
-	;mov	[code_buffer_here], rsi
+	test	rax, rax
+	cmovs	edi, eax
+	js	exit
 
 	ret
 
 init:
 	mov	[instruction_pointer], input_buffer
+	mov	[opcode_handler], OPCODE_HANDLER_INTERPRET
+	mov	[opcode_handler_invalid], OPCODE_HANDLER_INVALID
+	
 	mov	[return_stack_here], return_stack
 	mov	[return_stack_size], RETURN_STACK_SIZE
 	mov	[data_stack_here], data_stack
@@ -41,15 +40,14 @@ init:
 	mov	[code_buffer_here], code_buffer + VM_ADDRS_SIZE
 	mov	[code_buffer_size], CODE_BUFFER_SIZE
 	
-	mov	[opcode_handler], OPCODE_HANDLER_INTERPRET
-	mov	[opcode_handler_invalid], OPCODE_HANDLER_INVALID
-	
 	ret
 	
 read_boot_code:
 	mov	rsi, input_buffer
 	mov	edx, INPUT_BUFFER_SIZE
-	call	read
+	xor	edi, edi
+	xor	eax, eax
+	call	syscall_or_die
 
 	test	eax, eax
 	cmovz	edi, eax
@@ -58,32 +56,31 @@ read_boot_code:
 	ret
 
 entry $
-	call	mem_alloc_init
-
 	call	init
 	call	read_boot_code	
 	call	outer_interpreter
 
 
 instruction_pointer rq 1
+opcode_handler rq 1
+opcode_handler_invalid rq 1
+
 input_buffer rb INPUT_BUFFER_SIZE
+
 return_stack rb RETURN_STACK_SIZE
 return_stack_here rq 1
 return_stack_size rq 1
+
 data_stack rb DATA_STACK_SIZE
 data_stack_here rq 1
 data_stack_size rq 1
+
 opcode_map rb OPCODE_MAP_SIZE
 
 vm_addrs:
 vm_addr_opcode_handler_call rq 1
 times (VM_ADDRS_SIZE - ($ - vm_addrs)) rb 1
+
 code_buffer rb (CODE_BUFFER_SIZE - VM_ADDRS_SIZE)
 code_buffer_here rq 1
 code_buffer_size rq 1
-
-
-mem rq 1
-
-opcode_handler rq 1
-opcode_handler_invalid rq 1
