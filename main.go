b909266c31062a15f4b1c122031aeba236e7435c
Jon Herron <jon.herron@yahoo.com>
2025-02-06 14:34:41 -0500

    WIP

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "encoding/binary"
 import "fmt"
+import "unsafe"
+import "syscall"
+
+/*
+
+#include <stdint.h>
+#include <stdio.h>
+
+void interpret(int64_t *data_stack, uint8_t *code_buffer) {
+	((void (*)())code_buffer)(data_stack);
+	printf("OK? %d\n", *code_buffer);
+}
+
+*/
+import "C"
 
 func main() {
 	var data_stack [16]int64
@@ -16,14 +31,16 @@ func main() {
 
 	// mov ecx, ds[tos--]
 	code_buffer = append(code_buffer, 0xB9)
-	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
 	tos--
+	fmt.Println("ecx: ", data_stack[tos])
+	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
 	code_buffer = append(code_buffer, int32_buf...)
 
-	// mov ecx, ds[tos--]
+	// mov eax, ds[tos--]
 	code_buffer = append(code_buffer, 0xB8)
-	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
 	tos--
+	fmt.Println("eax: ", data_stack[tos])
+	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
 	code_buffer = append(code_buffer, int32_buf...)
 
 	// add eax, ecx
@@ -37,7 +54,24 @@ func main() {
 	// ret
 	code_buffer = append(code_buffer, 0xC3)
 
-	// call machine code
+	cb, err := syscall.Mmap(-1, 0, 4096, syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC, syscall.MAP_ANONYMOUS|syscall.MAP_PRIVATE)
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.Munmap(cb)
+
+	copy(cb, code_buffer)
 	
-	fmt.Println("blue: ", tos, len(code_buffer), len(int32_buf))
+	fmt.Println("blue: ", tos, data_stack[0], len(code_buffer), len(cb))
+	data_stack[0] = 111
+
+	// call machine code
+	C.interpret(
+		(*C.int64_t)(&data_stack[tos]),
+		(*C.uint8_t)(unsafe.Pointer(&cb[0])),
+	)
+
+	tos++
+	
+	fmt.Println("blue: ", tos, data_stack[0], len(code_buffer), len(cb))
 }
