2c19f27044907a53622d665a8905ffee60ebfdb2
Jon Herron <jon.herron@yahoo.com>
2025-02-06 17:17:06 -0500

    WIP

--- main.go
+++ main.go
@@ -9,18 +9,32 @@ import "syscall"
 
 #include <stdint.h>
 
-void interpret(int64_t *data_stack, uint8_t *code) {
+void interpret(uint64_t *data_stack, uint8_t *code) {
 	((void (*)())code)(data_stack);
 }
 
 */
 import "C"
 
+/*
+type Stack[T any] struct {
+	elems []T
+	i int
+}
+
+func NewStack[T any](cap int) Stack[T] {
+	return Stack[T]{
+		elems: make([]T, cap),
+	};
+}
+*/
+
 func main() {
-	data_stack := make([]int64, 16)
+	//data_stack := NewStack[int64](16)
+	data_stack := make([]uint64, 16)
 	tos := 0
 	here := 0
-	int32_buf := make([]byte, 4)
+	uint32_buf := make([]byte, 4)
 
 	code_buffer, err := syscall.Mmap(-1, 0, 4096,
 		syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC,
@@ -40,16 +54,16 @@ func main() {
 	code_buffer[here] = 0xB9
 	here++
 	tos--
-	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
-	copy(code_buffer[here:], int32_buf)
+	binary.LittleEndian.PutUint32(uint32_buf, uint32(data_stack[tos]))
+	copy(code_buffer[here:], uint32_buf)
 	here += 4
 
 	// mov eax, ds[tos--]
 	code_buffer[here] = 0xB8
 	here++
 	tos--
-	binary.LittleEndian.PutUint32(int32_buf, uint32(data_stack[tos]))
-	copy(code_buffer[here:], int32_buf)
+	binary.LittleEndian.PutUint32(uint32_buf, uint32(data_stack[tos]))
+	copy(code_buffer[here:], uint32_buf)
 	here += 4
 
 	// add eax, ecx
@@ -72,7 +86,7 @@ func main() {
 
 	// call machine code
 	C.interpret(
-		(*C.int64_t)(&data_stack[tos]),
+		(*C.uint64_t)(&data_stack[tos]),
 		(*C.uint8_t)(unsafe.Pointer(&code_buffer[0])),
 	)
 
