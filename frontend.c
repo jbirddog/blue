8d8a539720dd43cf7387662895ff8d23168b0b7b
Jon Herron <jon.herron@yahoo.com>
2025-02-09 10:55:42 -0500

    WIP

--- frontend.c
+++ frontend.c
@@ -1,9 +1,20 @@
 #include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "blue.h"
+
+static void append_lit_cmd(uint64_t lit, blue_ctx *ctx) {
+	size_t size = 1;
+
+	if (lit > UINT_MAX) size = 8;
+	else if (lit > USHRT_MAX) size = 4;
+	else if (lit > UCHAR_MAX) size = 2;
+	
+	blue_list_append(ctx->commands, c, { c->type = CMD_LIT; c->size = size; c->val = lit; });
+}
 
 void parse(blue_ctx *ctx) {
 	blue_list_append(ctx->blocks, b, {
@@ -27,9 +38,12 @@ void parse(blue_ctx *ctx) {
 		char *num_end;
 		uint64_t num = strtoll(tok, &num_end, 0);
 
+		// TODO: die
 		if (num_end != tok_end) break;
+
+		append_lit_cmd(num, ctx);
 		
-		fprintf(stderr, "num: %ld, tok: '%s' %ld\n", num, tok, tok_len);
+		fprintf(stderr, "num: %ld, tok: '%s' %ld\n", num, s, tok_len);
 		break;
 	}
 
