9074b319f6389e5bf107a94c2ec5659333181e76
Jon Herron <jon.herron@yahoo.com>
2025-05-15 19:46:23 -0400

    Better README.md generation

--- gen.mk
+++ gen.mk
@@ -1,5 +1,5 @@
 
-GEN_FILES = ops.tbl ops.md
+GEN_FILES = ops.tbl ops.md README.md
 
 ops.tbl: opcodes.inc
 	sed -rn "s/^op[NB]I?\top_([^,]+), ([0-9]), [^\t]+\t;\t(.*)/\1\t\2\t\3/p" $^ > $@
@@ -7,5 +7,5 @@ ops.tbl: opcodes.inc
 ops.md: ops.tbl
 	awk -v FS='\t' '{printf "| %02x | %s | %s | %s |\n", NR - 1, $$1, $$3, $$4}' ops.tbl > ops.md
 
-README.md: README_1.md ops.md README_2.md
-	cat README_1.md ops.md README_2.md > README.md
+README.md: README.md.tmpl README.sh ops.md
+	./README.sh > README.md
