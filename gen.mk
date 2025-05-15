fcc2050c5251baa2214a677eb978c579605d61c6
Jon Herron <jon.herron@yahoo.com>
2025-05-15 16:51:59 -0400

    Prep to generate README with ops table

--- gen.mk
+++ gen.mk
@@ -1,5 +1,6 @@
 
-gen-ops-tbl:
-	sed -rn "s/^op[NB]I?\top_([^,]+), ([0-9]), [^\t]+\t;\t(.*)/\1\t\2\t\3/p" opcodes.inc > ops.tbl
+ops.tbl: opcodes.inc
+	sed -rn "s/^op[NB]I?\top_([^,]+), ([0-9]), [^\t]+\t;\t(.*)/\1\t\2\t\3/p" $^ > $@
 
-.PHONY: gen-ops-tbl
+ops.md: ops.tbl
+	awk -v FS='\t' '{printf "| %02x | %s | %s | %s |\n", NR - 1, $$1, $$3, $$4}' ops.tbl > ops.md
