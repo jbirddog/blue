49a32a3926cf8db60095e25bd879d550635f14d6
Jon Herron <jon.herron@yahoo.com>
2025-05-19 18:17:26 -0400

    WIP

--- bluevm.asm
+++ bluevm.asm
@@ -3,8 +3,8 @@ format ELF64 executable 3
 
 CELL_SIZE = 8
 ELF_HEADERS_SIZE = 120
-VM_CODE_SIZE = 1024 - ELF_HEADERS_SIZE
 
+VM_CODE_SIZE = 1024 - ELF_HEADERS_SIZE
 RETURN_STACK_SIZE = 512
 DATA_STACK_SIZE = 512
 VM_OPCODE_TBL_SIZE = 2048
@@ -13,12 +13,13 @@ OPCODE_TBL_SIZE = VM_OPCODE_TBL_SIZE + EXT_OPCODE_TBL_SIZE
 INPUT_BUFFER_SIZE = 1024
 CODE_BUFFER_SIZE = 4096
 
-VM_BINARY_SIZE = VM_CODE_SIZE + \
+VM_MEM_SIZE = VM_CODE_SIZE + \
 	RETURN_STACK_SIZE + \
 	DATA_STACK_SIZE + \
-	CODE_BUFFER_SIZE + \
 	OPCODE_TBL_SIZE + \
-	INPUT_BUFFER_SIZE
+	INPUT_BUFFER_SIZE + \
+	CODE_BUFFER_SIZE + \
+	0
 
 OPCODE_HANDLER_COMPILE = opcode_handler_compile
 OPCODE_HANDLER_INTERPRET = opcode_handler_interpret
@@ -30,14 +31,14 @@ OPCODE_ENTRY_FLAG_BYTECODE = 1 shl 2
 
 segment readable writeable executable
 
-instruction_pointer dq 0
+instruction_pointer dq input_buffer
 
-opcode_handler dq 0
-opcode_handler_invalid dq 0
+opcode_handler dq OPCODE_HANDLER_INTERPRET
+opcode_handler_invalid dq OPCODE_HANDLER_INVALID
 
-return_stack_here dq 0
-data_stack_here dq 0
-code_buffer_here dq 0
+return_stack_here dq return_stack
+data_stack_here dq data_stack
+code_buffer_here dq code_buffer
 
 include "stack.inc"
 include "ops_code.inc"
@@ -56,17 +57,6 @@ syscall_or_die:
 	js	exit
 
 	ret
-
-init:
-	mov	[instruction_pointer], input_buffer
-	mov	[opcode_handler], OPCODE_HANDLER_INTERPRET
-	mov	[opcode_handler_invalid], OPCODE_HANDLER_INVALID
-	
-	mov	[return_stack_here], return_stack
-	mov	[data_stack_here], data_stack
-	mov	[code_buffer_here], code_buffer
-	
-	ret
 	
 read_boot_code:
 	mov	rsi, input_buffer
@@ -82,7 +72,6 @@ read_boot_code:
 	ret
 
 entry $
-	call	init
 	call	read_boot_code	
 	call	interpreter
 
@@ -98,4 +87,4 @@ return_stack rb RETURN_STACK_SIZE
 data_stack rb DATA_STACK_SIZE
 code_buffer rb CODE_BUFFER_SIZE
 
-assert ($ - $$) = VM_BINARY_SIZE
+assert ($ - $$) = VM_MEM_SIZE
