35e6e2632364c9e951ef5b519304b06b4ddd4dac
Jon Herron <jon.herron@yahoo.com>
2025-05-20 07:06:44 -0400

    Gen

--- lang/blasm/blasm.inc.sh
+++ lang/blasm/blasm.inc.sh
@@ -4,5 +4,5 @@ DIR="$( dirname -- "$( readlink -f -- "$0"; )"; )"
 OPS_TBL="$DIR/../../ops.tbl"
 
 awk -v OPS="$(awk -v FS='\t' '{printf "\t%s, %d, \\\\\\n", $1, $2}' $OPS_TBL)" \
-	'{gsub(/^_OPS_$/, OPS); print}' \
+	'BEGIN {print "; Generated from blasm.inc.tmpl"} {gsub(/^_OPS_$/, OPS); print}' \
 	$DIR/blasm.inc.tmpl
