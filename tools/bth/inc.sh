74540ee21862ec3264a5972c525f7b13c7b1299c
Jon Herron <jon.herron@yahoo.com>
2025-05-29 07:38:36 -0400

    WIP

--- tools/bth/inc.sh
+++ tools/bth/inc.sh
@@ -3,6 +3,14 @@
 DIR="$( dirname -- "$( readlink -f -- "$0"; )"; )"
 OPS_TBL="$DIR/ops_$WHICH.tbl"
 
-awk -v OPS="$(awk -v FS='\t' '{printf "\t%s, %d, \\\\\\n", $1, $2}' $OPS_TBL)" \
-	'BEGIN {print "; Generated from inc.tmpl"} {gsub(/^_OPS_$/, OPS); print}' \
+awk \
+	-v OPS="$(awk -v FS='\t' '{printf "\t%s, %d, \\\\\\n", $1, $2}' $OPS_TBL)" \
+	-v OFFSET="$OFFSET" \
+	'BEGIN {print "; Generated from inc.tmpl"}
+	{
+		gsub(/^_OPS_$/, OPS);
+		gsub(/_OFFSET_/, OFFSET);
+		print
+	}
+	' \
 	$DIR/inc.tmpl
