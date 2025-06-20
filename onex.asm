7851ac48378a530bb05a4612c06aaf28a06c7a07
Jon Herron <jon.herron@yahoo.com>
2025-06-20 12:07:35 -0400

    WIP

--- onex.asm
+++ onex.asm
@@ -2,6 +2,8 @@ format ELF64 executable 3
 
 segment readable writeable executable
 
+OP_SIZE = 8
+
 ; from https://flatassembler.net/docs.php?article=fasmg_manual
 macro show description,value
 	repeat 1, d:value
@@ -9,22 +11,33 @@ macro show description,value
 	end repeat
 end macro
 
-CELL_SIZE = 8
-
-
 ;;
-_tbl:
-_0:
+
+op_tbl:
+
+macro op n
+op_##n = $ - op_tbl
+end macro
+
+macro end_op
+	times (OP_SIZE - (($ - op_tbl) mod OP_SIZE)) db 0
+end macro
+
+op	cpb	;	( -- )	copy byte from src to dst
 	movsb
 	ret
-	times (CELL_SIZE - ($-_0)) db 0
-_1:
+end_op
+
+op	cpd	;	( -- )	copy dword from src to dst
 	movsd
 	ret
-	times (CELL_SIZE - ($-_1)) db 0
-_2:
+end_op
+
+op	fin	;	( -- )	TMP
 	jmp	_dst
-	times (CELL_SIZE - ($-_2)) db 0
+end_op
+
+assert (($ - op_tbl) mod OP_SIZE) = 0
 ;;
 
 entry $
@@ -35,8 +48,8 @@ entry $
 	xor	eax, eax
 	
 	lodsb
-	lea	rdx, [_tbl+(rax*CELL_SIZE)]
-	call	rdx
+	add	rax, op_tbl
+	call	rax
 
 	jmp	.loop
 
@@ -45,22 +58,20 @@ show "onex code size: ", ($ - $$)
 _src:
 
 ; mov eax, 0x3C
-db	0x00, 0xB8
-db	0x01, 0x3C, 0x00, 0x00, 0x00
+db	op_cpb, 0xB8
+db	op_cpd, 0x3C, 0x00, 0x00, 0x00
 
 ; mov edi, 0x0B
-db	0x00, 0xBF
-db	0x01, 0x0B, 0x00, 0x00, 0x00
+db	op_cpb, 0xBF
+db	op_cpd, 0x0B, 0x00, 0x00, 0x00
 
 ; syscalll
-db	0x00, 0x0F
-db	0x00, 0x05
+db	op_cpb, 0x0F
+db	op_cpb, 0x05
 
 ; fin
-db	0x02
+db	op_fin
 
-;0:  b8 3c 00 00 00          mov    eax,0x3c
-;5:  bf 0b 00 00 00          mov    edi,0xb
-;a:  0f 05                   syscall 
+show "bytecode size: ", ($ - _src)
 
 _dst rb 1024
