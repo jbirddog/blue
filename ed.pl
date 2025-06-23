f1c029bbdcb3272b299a8fa8fd20a45080648bcb
Jon Herron <jon.herron@yahoo.com>
2025-06-23 08:43:19 -0400

    WIP

--- ed.pl
+++ ed.pl
@@ -12,6 +12,20 @@ use constant {
 	BC_COMP_BYTE => 0x03,
 };
 
+my @bc_lens = qw(
+	0,
+	8,
+	8,
+	1,
+);
+
+my @bc_fmts = (
+	"",
+	"\x1B[31m%s ",
+	"\x1B[31m%s ",
+	"\x1B[31m%s ",
+);
+
 my $bc_file = "test.blk";
 my $bc = "";
 
@@ -21,22 +35,33 @@ if (-e $bc_file) {
 	close $in;
 }
 
+$bc = chr(BC_DEFINE_WORD) . "exit\x00\x00\x00\x00";
+
 $bc .= "\x00" x (BLK_SIZE - length($bc));
 
 my $here = 0;
 my $display = "\x1B[2J";
 
 while ($here < length($bc)) {
-	my $b = substr($bc, $here, 1);
-
+	my $b = ord(substr($bc, $here, 1));
+	++$here;
 	last if $b == BC_FIN;
 
-	++$here;
+	my $fmt = @bc_fmts[1];
+	my $len = @bc_lens[$b];
+
+	my $data = substr($bc, $here, $len);
+	$here += $len;
+	
+	$display .= sprintf($fmt, $data);
+
+	last;
 };
+
+$display .= "\x1B[0m\n";
 
 print $display;
 
 open my $out, ">:raw", $bc_file;
 print $out $bc;
 close $out;
-
