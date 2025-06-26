116d259c6ac53eff3c4e4cc2d28d04a5fbb1a01f
Jon Herron <jon.herron@yahoo.com>
2025-06-26 14:52:35 -0400

    Go back to mmap

--- onex.asm
+++ onex.asm
@@ -1,7 +1,7 @@
 ;
 ; TODOs:
 ;
-; * Put the data stack in block 0 with bc_tbl
+; * Put data stack in mmap'd page
 ; * Dict entry 0x00 can have not found handler
 ; * Limit dict entry cell 1 values to 7 chars, use 1 byte for flags, etc
 ; * Need BC_EXEC_NUM
@@ -9,7 +9,7 @@
 ; * Once editor is prototyped, port to onex
 ; * Port bth to onex
 ;   - Block 1 for bth logic
-;   - Concat with Block 0, mmap file from argv[1] and set src
+;   - Concat with block 0, mmap file from argv[1] and set src
 ;   - Mmap for test output
 ;
 ; - Keep in mind there is no ip - just compiling byte code top to bottom
@@ -20,18 +20,30 @@
 
 format ELF64 executable 3
 
-segment readable writeable executable
+segment readable executable
 
-BLK_SIZE = 1024
-CELL_SIZE = 8
-DICT_ENTRY_SIZE = CELL_SIZE * 2
-ELF_HEADERS_SIZE = 120
+MAP_ANONYMOUS = 32
+MAP_PRIVATE = 2
+
+PROT_READ = 1
+PROT_WRITE = 2
+PROT_EXEC = 4
+
+PROT_RW = PROT_READ or PROT_WRITE
+PROT_RWX = PROT_RW or PROT_EXEC
 
 REG_SRC = rsi
 REG_DST = rdi
 REG_LAST = r12
 
 SYS_EXIT = 60
+SYS_MMAP = 9
+
+BLK_SIZE = 1024
+CELL_SIZE = 8
+DICT_ENTRY_SIZE = CELL_SIZE * 2
+ELF_HEADERS_SIZE = 120
+PAGE_SIZE = 4096
 
 ; from https://flatassembler.net/docs.php?article=fasmg_manual
 macro show description,value
@@ -98,13 +110,22 @@ dq	core_define
 dq	bc_exec_word
 dq	bc_comp_byte
 
-entry $	
+entry $
+	xor	edi, edi
+	mov	esi, PAGE_SIZE
+	mov	edx, PROT_RWX
+	mov	r10d, MAP_ANONYMOUS or MAP_PRIVATE
+	mov	r8d, -1
+	xor	r9d, r9d
+	mov	eax, SYS_MMAP
+	syscall
+	
+	mov	REG_DST, rax
+	lea	REG_LAST, [rax + ((BLK_SIZE shl 1) + DICT_ENTRY_SIZE)]
 	mov	REG_SRC, _src
-	mov	REG_LAST, _dict
-	mov	REG_DST, _dst
 
 	call	core_load
-
+	
 	xor	edi, edi
 	mov	eax, SYS_EXIT
 	syscall
@@ -113,7 +134,7 @@ show "code size: ", ($ - $$)
 times (BLK_SIZE - ($ - $$ + ELF_HEADERS_SIZE)) db 0
 
 _src:
-times BLK_SIZE db 0
 
-_dict rb BLK_SIZE
-_dst rb BLK_SIZE
+file "test.blk"
+
+times (BLK_SIZE - ($ - _src)) db 0
