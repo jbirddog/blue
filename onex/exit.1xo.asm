aa95ed8664985671daff9aaf8583a9301e3c697a
Jon Herron <jon.herron@yahoo.com>
2025-07-11 07:40:02 -0400

    WIP

--- onex/exit.1xo.asm
+++ onex/exit.1xo.asm
@@ -1,22 +1,31 @@
 include "bc.inc"
 
 db	BC_ED_NL
+db	BC_DEFINE_WORD
+dq	"dil!"
 
+; 40 b7 07		mov    dil,0x7
+db	BC_COMP_BYTE, 0x40
+db	BC_COMP_BYTE, 0xB7
+
+db	BC_ED_NL
+db	BC_DEFINE_WORD
+dq	"status"
+
+db	BC_COMP_BYTE, 0x07
+
+db	BC_ED_NL
 db	BC_DEFINE_WORD
 dq	"exit"
-
-; 31 c0			xor    eax,eax
-db	BC_COMP_BYTE, 0x31
-db	BC_COMP_BYTE, 0xC0
 
 ; b0 3c			mov    al,0x3c
 db	BC_COMP_BYTE, 0xB0
 db	BC_COMP_BYTE, 0x3C
 
-; 40 b7 03		mov    dil,0x7
-db	BC_COMP_BYTE, 0x40
-db	BC_COMP_BYTE, 0xB7
-db	BC_COMP_BYTE, 0x07
+db	BC_ED_NL
+
+db	BC_DEFINE_WORD
+dq	"syscall"
 
 ; 0f 05			syscall
 db	BC_COMP_BYTE, 0x0F
@@ -30,6 +39,16 @@ dq	"dstsz"
 
 db	BC_REF_WORD
 dq	"elfbsz"
+
+db	BC_EXEC_WORD
+dq	"!"
+db	BC_ED_NL
+
+db	BC_EXEC_WORD
+dq	"dstsz"
+
+db	BC_REF_WORD
+dq	"elfmsz"
 
 db	BC_EXEC_WORD
 dq	"!"
