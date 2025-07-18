388d6d95095821a139eaec058b139f7af7597813
Jon Herron <jon.herron@yahoo.com>
2025-07-18 16:28:08 -0400

    WIP

---
+++ onex/samples/hello/elf.fin.1xo.asm
@@ -0,0 +1,23 @@
+include "bc.inc"
+
+db	BC_ED_NL
+
+db	BC_DOLLAR_CADDR
+db	BC_WORD_CADDR
+dq	"elfhdrs"
+db	BC_SUB
+
+db	BC_DUP
+
+db	BC_WORD_CADDR
+dq	"elfbsz"
+db	BC_SET
+
+db	BC_WORD_CADDR
+dq	"elfmsz"
+db	BC_SET
+
+db	BC_ED_NL
+db	BC_ED_NL
+
+db	BC_FIN
