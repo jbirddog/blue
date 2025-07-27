d913de16c46672dce0e637ab862b51e2d93b5378
Jon Herron <jon.herron@yahoo.com>
2025-07-27 14:20:40 -0400

    WIP

--- examples/helloworld/helloworld.bo.asm
+++ examples/helloworld/helloworld.bo.asm
@@ -8,17 +8,6 @@ db	BC_NUM_COMP
 db	"orld!!!", 10
 
 db	BC_ED_NL
-
-db	BC_WORD_DEFINE
-dq	"msglen"
-db	BC_WORD_CADDR
-dq	"msglen"
-db	BC_WORD_CADDR
-dq	"msg"
-db	BC_SUB
-db	BC_WORD_END
-
-db	BC_ED_NL
 db	BC_ED_NL
 
 db	BC_WORD_DEFINE
@@ -32,10 +21,14 @@ db	BC_ED_NL
 
 db	BC_WORD_RADDR
 dq	"msg"
-db	BC_WORD_INTERP
-dq	"msglen"
+db	BC_DOLLAR_CADDR
+db	BC_WORD_CADDR
+dq	"msg"
+db	BC_SUB
 db	BC_WORD_INTERP
 dq	"print"
+
+db	BC_ED_NL
 
 db	BC_WORD_INTERP
 dq	"bye"
