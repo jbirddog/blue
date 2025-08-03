83236e798ae0d068495d43da1bbd1992995b52c0
Jon Herron <jon.herron@yahoo.com>
2025-08-02 21:58:41 -0400

    More commas

--- blue/ops.bo.asm
+++ blue/ops.bo.asm
@@ -178,6 +178,10 @@ db	BC_DSP_NL
 db	BC_WORD_DEFINE
 dq	"comma_b"
 db	BC_WORD_RCALL
+dq	"dspop"
+db	BC_WORD_INTERP
+dq	"stosb"
+db	BC_WORD_RCALL
 dq	"next"
 db	BC_WORD_END
 
@@ -186,6 +190,10 @@ db	BC_DSP_NL
 db	BC_WORD_DEFINE
 dq	"comma_w"
 db	BC_WORD_RCALL
+dq	"dspop"
+db	BC_WORD_INTERP
+dq	"stosw"
+db	BC_WORD_RCALL
 dq	"next"
 db	BC_WORD_END
 
@@ -193,6 +201,10 @@ db	BC_DSP_NL
 
 db	BC_WORD_DEFINE
 dq	"comma_d"
+db	BC_WORD_RCALL
+dq	"dspop"
+db	BC_WORD_INTERP
+dq	"stosd"
 db	BC_WORD_RCALL
 dq	"next"
 db	BC_WORD_END
--- tests/bc/comma.bo.asm
+++ tests/bc/comma.bo.asm
@@ -1,6 +1,19 @@
 include "b.inc"
 
 db	BC_NUM_PUSH
-db	"Blue...", 10
+dq	"AAAAAAAA"
 db	BC_COMMA
+
+db	BC_NUM_PUSH
+dq	"BBBB"
+db	BC_COMMA_D
+
+db	BC_NUM_PUSH
+dq	"CC"
+db	BC_COMMA_W
+
+db	BC_NUM_PUSH
+dq	0x0A
+db	BC_COMMA_B
+
 db	BC_FIN
