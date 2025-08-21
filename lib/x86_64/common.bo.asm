b2c49952c9a99127b1a3e8d83ea562dbf59bdd41
Jon Herron <jon.herron@yahoo.com>
2025-08-20 21:38:25 -0400

    More werk

--- lib/x86_64/common.bo.asm
+++ lib/x86_64/common.bo.asm
@@ -89,17 +89,15 @@ db	BC_COMMA_W
 db	BC_WORD_END
 
 db	BC_WORD_DEFINE
+dq	"stosq"
+db	BC_WORD_INTERP
+dq	"rex.w"
+
+db	BC_WORD_DEFINE
 dq	"stosd"
 db	BC_NUM_PUSH
 dq	0xAB
 db	BC_COMMA_B
-db	BC_WORD_END
-
-db	BC_WORD_DEFINE
-dq	"stosq"
-db	BC_NUM_PUSH
-dq	0xAB48
-db	BC_COMMA_W
 db	BC_WORD_END
 
 db	BC_DSP_NL
