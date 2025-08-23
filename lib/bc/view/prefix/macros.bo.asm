ad8be9ef5a67cef5e480dcd614e91cf2fc980fb0
Jon Herron <jon.herron@yahoo.com>
2025-08-23 19:39:29 -0400

    Cleanup

--- lib/bc/view/prefix/macros.bo.asm
+++ lib/bc/view/prefix/macros.bo.asm
@@ -1,20 +1,6 @@
 include "bc.inc"
 
 db	BC_WORD_DEFINE
-dq	"reset"
-db	BC_WORD_END
-
-db	BC_WORD_DEFINE
-dq	"++"
-db	BC_WORD_END
-
-db	BC_WORD_DEFINE
-dq	"shows"
-db	BC_WORD_END
-
-db	BC_DSP_NL
-
-db	BC_WORD_DEFINE
 dq	"red"
 db	BC_NUM_PUSH
 dq	":"
@@ -28,30 +14,46 @@ db	BC_WORD_END
 
 db	BC_WORD_DEFINE
 dq	"yellow"
+db	BC_NUM_PUSH
+dq	"#"
+db	BC_WORD_INTERP
+dq	"bout"
 db	BC_WORD_END
 
 db	BC_DSP_NL
 
 db	BC_WORD_DEFINE
 dq	"magenta"
+db	BC_NUM_PUSH
+dq	"@"
+db	BC_WORD_INTERP
+dq	"bout"
 db	BC_WORD_END
 
 db	BC_WORD_DEFINE
 dq	"cyan"
+db	BC_NUM_PUSH
+dq	"~"
+db	BC_WORD_INTERP
+dq	"bout"
 db	BC_WORD_END
 
 db	BC_WORD_DEFINE
 dq	"white"
+db	BC_NUM_PUSH
+dq	"&"
+db	BC_WORD_INTERP
+dq	"bout"
 db	BC_WORD_END
 
 db	BC_DSP_NL
 
 db	BC_WORD_DEFINE
-dq	"bold"
+dq	"reset"
 db	BC_WORD_END
 
 db	BC_WORD_DEFINE
-dq	"italic"
+dq	"shows"
 db	BC_WORD_END
 
 db	BC_DSP_NL
