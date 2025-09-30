e8ca88cb0975bf828f9be58556c38bbd1fd9fd14
Jon Herron <jon.herron@yahoo.com>
2025-09-29 20:16:09 -0400

    More aarch64 example work

--- examples/aarch64/exit/exit.bo.asm
+++ examples/aarch64/exit/exit.bo.asm
@@ -1,15 +1,30 @@
 include "bc.inc"
 
+db	BC_WORD_INTERP
+dq	"w"
 db	BC_NUM_PUSH
-dq	0xD280'0160
-db	BC_COMMA_D
+dq	0x00
+db	BC_NUM_PUSH
+dq	0x00
+db	BC_WORD_INTERP
+dq	"movz"
+
+db	BC_DSP_NL
+
+db	BC_WORD_INTERP
+dq	"w"
+db	BC_NUM_PUSH
+dq	0x08
+db	BC_NUM_PUSH
+dq	0x5D
+db	BC_WORD_INTERP
+dq	"movz"
+
+db	BC_DSP_NL
 
 db	BC_NUM_PUSH
-dq	0x5280'0BA8
-db	BC_COMMA_D
-
-db	BC_NUM_PUSH
-dq	0xD400'0001
-db	BC_COMMA_D
+dq	0x00
+db	BC_WORD_INTERP
+dq	"svc"
 
 db	BC_DSP_NL
